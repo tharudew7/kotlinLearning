5f0a81989c1d8ab464893147158ef1074023de8d
tharudew7 <73303151+tharudew7@users.noreply.github.com>
2023-03-08 21:25:17 +0530

    Commit 2

---
+++ app/src/main/java/com/example/kotlinlearning1/classes and objects.kt
@@ -0,0 +1,33 @@
+package com.example.kotlinlearning1
+
+fun main() {
+    var object2 = Person("Sithma", "Hirushi")
+    var object1 = Person(lastName = "De Silva")
+
+    object2.favoriteColor = "Blue"
+    object1.favoriteColor = "Pink"
+
+    object2.myFavoriteColor()
+    object1.myFavoriteColor()
+}
+
+class Person(firstName: String = "Tharushi", lastName: String = "Dharmasiri") {
+
+    var firstName : String? = null
+    init {
+        println("My name is $firstName $lastName")
+        this.firstName = firstName
+    }
+
+    // Member variable or properties
+    var age: Int? = null
+    var favoriteColor = "Red"
+
+
+    // Member Function
+    fun myFavoriteColor() {
+        println("${firstName} favorite color is $favoriteColor")
+    }
+}
+
+
